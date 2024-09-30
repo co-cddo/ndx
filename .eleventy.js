a9a62c39e7bf84bf8298934ff7ce8f1fd1c655a2
Chris Nesbitt-Smith <chris@cns.me.uk>
2024-09-30 09:40:38 +0100

    eleventy config

---
+++ .eleventy.js
@@ -0,0 +1,52 @@
+const govukEleventyPlugin = require("@x-govuk/govuk-eleventy-plugin")
+const fs = require("fs")
+const util = require("util")
+
+function gitRev() {
+  const rev = fs.readFileSync(".git/HEAD").toString().trim()
+  if (rev.indexOf(":") === -1) return rev
+  else
+    return fs
+      .readFileSync(".git/" + rev.substring(5))
+      .toString()
+      .trim()
+}
+function gitSHA() {
+  return gitRev().slice(0, 8)
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+    header: {
+      productName: `National Digital Exchange <strong class="govuk-tag govuk-phase-banner__content__tag">Alpha</strong>`,
+    },
+    footer: {
+      meta: {
+        text: `Page built from <a href="https://github.com/co-cddo/ndx/commit/${gitRev()}">${gitSHA()}</a> at ${new Date().toISOString()}`,
+      },
+    },
+  })
+
+  eleventyConfig.addFilter("console", function (value) {
+    const str = util.inspect(value)
+    return `<code style="white-space: pre-wrap;">${decodeURIComponent(str)}</code>;`
+  })
+
+  eleventyConfig.addPassthroughCopy("./src/assets")
+  eleventyConfig.addPassthroughCopy("./src/robots.txt")
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/govuk-frontend/dist/govuk/assets/images": "./assets/images",
+    "./node_modules/govuk-frontend/dist/govuk/assets/fonts": "./assets/fonts",
+  })
+
+  return {
+    pathPrefix: process.env.PATH_PREFIX || "/",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dir: {
+      input: "./src",
+      layouts: "../node_modules/@x-govuk/govuk-eleventy-plugin/layouts",
+    },
+  }
+}

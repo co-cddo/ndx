adfd61c5a61c7ff1b8885f99a7323c02e71bd044
Chris Nesbitt-Smith <chris@cns.me.uk>
2025-05-21 16:00:47 +0100

    bump versions (#191)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,9 @@
-const govukEleventyPlugin = require("@x-govuk/govuk-eleventy-plugin")
-const { EleventyRenderPlugin } = require("@11ty/eleventy")
-const fs = require("fs")
-const util = require("util")
+import { govukEleventyPlugin } from "@x-govuk/govuk-eleventy-plugin"
+import { EleventyRenderPlugin } from "@11ty/eleventy"
+import fs from "fs"
+import util from "util"
 
-const pluginMermaid = require("@kevingimbel/eleventy-plugin-mermaid")
+import pluginMermaid from "@kevingimbel/eleventy-plugin-mermaid"
 
 function gitRev() {
   const rev = fs.readFileSync(".git/HEAD").toString().trim()
@@ -42,11 +42,11 @@ function extractContent(markdown, start, end) {
   return ""
 }
 
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin)
-  eleventyConfig.addPlugin(pluginMermaid)
 
   eleventyConfig.addPlugin(govukEleventyPlugin, {
+    rebrand: true,
     header: {
       productName: `National Digital Exchange <strong class="govuk-tag govuk-phase-banner__content__tag">Alpha</strong>`,
       search: {
@@ -55,9 +55,9 @@ module.exports = function (eleventyConfig) {
       },
     },
     stylesheets: ["/assets/styles.css"],
-    navigation: {
+    serviceNavigation: {
       home: "/",
-      items: [
+      navigation: [
         { text: "Home", href: "/" },
         { text: "About", href: "/About/Digital-Backbone/" },
         {
@@ -84,62 +84,7 @@ module.exports = function (eleventyConfig) {
       },
     },
   })
-
-  eleventyConfig.addFilter("console", function (value) {
-    const str = util.inspect(value)
-    return `<code style="white-space: pre-wrap;">${decodeURIComponent(str)}</code>;`
-  })
-
-  eleventyConfig.addPassthroughCopy("./src/assets")
-  eleventyConfig.addPassthroughCopy("./src/robots.txt")
-  eleventyConfig.addPassthroughCopy({
-    "./node_modules/govuk-frontend/dist/govuk/assets/images": "./assets/images",
-    "./node_modules/govuk-frontend/dist/govuk/assets/fonts": "./assets/fonts",
-  })
-
-  eleventyConfig.addCollection("news", (collection) =>
-    collection.getFilteredByGlob("src/discover/news/**/*.md", "!**/index.md").map(useExternalUrl),
-  )
-  eleventyConfig.addCollection("event", (collection) =>
-    collection.getFilteredByGlob("src/discover/events/**/*.md", "!**/index.md").map(useExternalUrl),
-  )
-  eleventyConfig.addCollection("casestudy", (collection) =>
-    collection.getFilteredByGlob("src/discover/case-studies/**/*.md", "!**/index.md").map(useExternalUrl),
-  )
-
-  eleventyConfig.addCollection("catalog", (collection) =>
-    collection
-      .getFilteredByGlob("src/catalog/**/*.md", "!**/index.md", "!**/tags.md")
-      .map(useExternalUrl)
-      .sort((a, b) => {
-        // Sort alphabetically by title
-        return a.data.title.localeCompare(b.data.title)
-      }),
-  )
-
-  // Add collection for reviews
-  eleventyConfig.addCollection("reviews", (collection) => {
-    // Log out what we're finding for debugging
-    const reviews = collection.getFilteredByGlob("./src/reviews/**/*.md")
-    console.log("Found " + reviews.length + " reviews")
-
-    return reviews.sort((a, b) => {
-      // Sort by date, newest first
-      return new Date(b.data.date) - new Date(a.data.date)
-    })
-  })
-
-  // Add collection for product assessments
-  eleventyConfig.addCollection("productAssessments", (collection) => {
-    return collection.getFilteredByGlob("./src/product-assessments/**/*.md").sort((a, b) => {
-      // Sort by date, newest first, then by title
-      if (b.data.date !== a.data.date) {
-        return new Date(b.data.date) - new Date(a.data.date)
-      } else {
-        return a.data.title.localeCompare(b.data.title)
-      }
-    })
-  })
+  eleventyConfig.addPlugin(pluginMermaid)
 
   eleventyConfig.addShortcode("remoteInclude", async function (url, start, end) {
     url = url.replace("https://github.com", "https://cdn.jsdelivr.net/gh").replace("/blob/", "@")
@@ -153,15 +98,60 @@ module.exports = function (eleventyConfig) {
     return f
   })
 
+  eleventyConfig.addFilter(
+    "console",
+    (value) => `<code style="white-space: pre-wrap;">${decodeURIComponent(util.inspect(value))}</code>;`,
+  )
+
+  eleventyConfig.addCollection("catalog", (collection) =>
+    collection
+      .getFilteredByGlob("src/catalog/**/*.md", "!**/index.md", "!**/tags.md")
+      .map(useExternalUrl)
+      .sort((a, b) => a.data.title.localeCompare(b.data.title)),
+  )
+
+  eleventyConfig.addCollection("reviews", (collection) =>
+    collection.getFilteredByGlob("./src/reviews/**/*.md").sort((a, b) => new Date(b.data.date) - new Date(a.data.date)),
+  )
+
+  eleventyConfig.addCollection("news", (collection) =>
+    collection.getFilteredByGlob("src/discover/news/**/*.md", "!**/index.md").map(useExternalUrl),
+  )
+  eleventyConfig.addCollection("event", (collection) =>
+    collection.getFilteredByGlob("src/discover/events/**/*.md", "!**/index.md").map(useExternalUrl),
+  )
+  eleventyConfig.addCollection("casestudy", (collection) =>
+    collection.getFilteredByGlob("src/discover/case-studies/**/*.md", "!**/index.md").map(useExternalUrl),
+  )
+
+  eleventyConfig.addCollection("productAssessments", (collection) =>
+    collection
+      .getFilteredByGlob("./src/product-assessments/**/*.md")
+      .sort((a, b) => new Date(b.data.date) - new Date(a.data.date)),
+  )
+
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/govuk-frontend/dist/govuk/assets/images": "./assets/images",
+    "./node_modules/govuk-frontend/dist/govuk/assets/fonts": "./assets/fonts",
+    "./node_modules/govuk-frontend/dist/govuk/assets": "./assets",
+  })
+  eleventyConfig.addPassthroughCopy("./src/assets")
+
   return {
-    pathPrefix: process.env.PATH_PREFIX || "/",
     dataTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     dir: {
       input: "./src",
-      layouts: "../node_modules/@x-govuk/govuk-eleventy-plugin/layouts",
       includes: "_includes",
+    },
+    nunjucksEnvironmentOptions: {
+      paths: [
+        "src/_includes",
+        "node_modules/govuk-frontend/dist",
+        "node_modules/govuk-frontend/dist/govuk",
+        "node_modules/govuk-frontend/dist/govuk/components",
+      ],
     },
   }
 }

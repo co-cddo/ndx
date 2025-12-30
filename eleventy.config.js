5c95821489adf0e1c8bfce366439d543076cedff
Chris Nesbitt-Smith <chris@cns.me.uk>
2025-12-30 18:44:29 +0000

    feat: use upstream phase banner config from govuk-eleventy-plugin v8.2.0 (#341)
    Upstream PR x-govuk/govuk-eleventy-plugin#417 added native phase banner
    support via header.phaseBanner config option. This removes our local
    template customization and uses the plugin's built-in feature.
    
    Changes:
    - Update @x-govuk/govuk-eleventy-plugin to ^8.2.0
    - Add phaseBanner config to eleventy.config.js header section
    - Remove phase banner macro and rendering from custom template.njk
    - Add CSS fix to apply govuk-width-container styling to phase banner
    - Remove unused scripts config option (plugin doesn't support it)

--- eleventy.config.js
+++ eleventy.config.js
@@ -102,13 +102,18 @@ export default function (eleventyConfig) {
     titleSuffix: "ALPHA",
     header: {
       productName: "National Digital Exchange",
+      phaseBanner: {
+        tag: {
+          text: "Alpha",
+        },
+        html: 'This is a new service. Help us improve it and <a class="govuk-link" href="mailto:ndx@dsit.gov.uk">give your feedback by email</a>.',
+      },
       search: {
         indexPath: "/search.json",
         sitemapPath: "/sitemap",
       },
     },
     stylesheets: ["/assets/styles.css"],
-    scripts: ["/assets/try.bundle.js"],
     serviceNavigation: {
       home: "/",
       navigation: [
